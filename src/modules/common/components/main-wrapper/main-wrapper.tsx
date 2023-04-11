987a0e77c0d9afc5c647acfc6512dd33cfc84e79
Olevia Yuen <aivelo@icloud.com>
2023-04-11 17:23:55 +0800

    implemented background color feature flag

--- src/modules/common/components/main-wrapper/main-wrapper.tsx
+++ src/modules/common/components/main-wrapper/main-wrapper.tsx
@@ -2,8 +2,14 @@ import { ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
+  backgroundColorFlag: boolean;
 };
 
-export const MainWrapper = ({ children }: Props) => {
-  return <div>{children}</div>;
+export const MainWrapper = (Props: Props) => {
+  if (Props.backgroundColorFlag) {
+    return <div style={{ backgroundColor: 'pink' }}>{Props.children}</div>;
+  }
+  return <div>{Props.children}</div>;
 };
+
+export default MainWrapper;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
+import { MainWrapper } from '../modules/common/components/main-wrapper/main-wrapper';
 
 export default function Home() {
   return (
-    <div>
+    <MainWrapper>
       <Link href='/todo'>
         <button>Start todo list</button>
       </Link>
-    </div>
+    </MainWrapper>
   );
 }

b932e18b98720d239e3ab5ee07a58d72a4679276
oleviacode <103043701+oleviacode@users.noreply.github.com>
2023-04-13 16:17:13 +0800

    Quick fex (#3)
    * installed configcat and styled-components
    
    * added eslint rules
    
    * implemented background color feature flag
    
    * adding github actions
    
    * added test to yml
    
    * removed container from yml
    
    * better naming for yml
    
    * testing yml
    
    * added husky
    
    * added husky
    
    * renamed to ci
    
    * removed main wrapper
    
    * moved styles to fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import Link from 'next/link';
-import { MainWrapper } from '../modules/common/components/main-wrapper/main-wrapper';
 
 export default function Home() {
   return (
-    <MainWrapper>
-      <Link href='/todo'>
-        <button>Start todo list</button>
-      </Link>
-    </MainWrapper>
+    <Link href='/todo'>
+      <button>Start todo list</button>
+    </Link>
   );
 }
